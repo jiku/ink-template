8460e1d1fd36011d1388c301c7ed7fd1318aa489
B.E. Henriksen <e@jiku.ca>
2019-02-12 23:41:52 +0100

    Initial commit

--- README.md
+++
@@ -1,2 +0,0 @@
-# ink-template
-Created with CodeSandbox
---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+import { render, Color } from "ink";
+
+const App = () => {
+  return <Color green>Hi</Color>;
+};
+
+render(<App />);
